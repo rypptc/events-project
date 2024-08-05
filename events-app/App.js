7453f81841f00f26cb65eef23341776d9596ec40
rypptc <rypptc@gmail.com>
2024-08-05 01:24:32 -0400

    add TextInput

--- events-app/App.js
+++ events-app/App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View, Platform, ScrollView } from 'react-native';
+import {useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, Platform, ScrollView, TextInput } from 'react-native';
 
 export default function App() {
+	const [text, setText] = useState("")
   return (
     <SafeAreaView>
       <Text style={{fontSize:32}}>hello world</Text>
       <Text style={{fontSize:24}}>Platform: {Platform.OS === 'ios' ? 'iOS' : 'Android'}</Text>
       <ScrollView>
         <View style={{backgroundColor: 'pink', height: 1600, padding: 20}}>
-          <Text>hello world</Text>
+          <TextInput 
+	          defaultValue={text}
+	          onChangeText={txt=>setText(txt)}
+          />
         </View>
       </ScrollView>
       <StatusBar style="dark" />
