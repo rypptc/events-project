e0e2c3aac4ac2ccb46b6aee7b62c903fa71b456a
rypptc <rypptc@gmail.com>
2024-08-05 01:33:19 -0400

    update App.js
    styles, buttons, touchable opacity

--- events-app/App.js
+++ events-app/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import {useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, View, Platform, ScrollView, TextInput } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Platform, ScrollView, TextInput, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
 	const [text, setText] = useState("")
@@ -13,7 +13,13 @@ export default function App() {
           <TextInput 
 	          defaultValue={text}
 	          onChangeText={txt=>setText(txt)}
+            style={{borderWidth: 1, padding: 10}}
           />
+          <Button title="press me" onPress={()=> console.log("Hello World!!")} />
+          <TouchableOpacity style={{ padding: 10, backgroundColor: "purple", width: 150, height: 200, justifyContent: "center", alignItems: "center" }} onPress={()=> console.log("Hello World 2")}>
+            <Text>Click me too</Text>
+          </TouchableOpacity>
+
         </View>
       </ScrollView>
       <StatusBar style="dark" />
