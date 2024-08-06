e2db885dbfdd2ad1e47a833125eb5735575602fe
rypptc <rypptc@gmail.com>
2024-08-06 09:56:45 -0400

    status bar light, options header style

--- events-app/App.js
+++ events-app/App.js
@@ -2,14 +2,14 @@ import './utils/gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { HomeStack } from './navigation/stack';
 import { MyDrawer } from './navigation/drawer';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
-
   return (
     <NavigationContainer>
       {/* <HomeStack /> */}
       <MyDrawer />
+      <StatusBar style='light' />
     </NavigationContainer>
   );
 }
-
--- events-app/navigation/options.js
+++ events-app/navigation/options.js
@@ -1,6 +1,9 @@
 export const navOptions = (nav) => {
     return {
-      headerTintColor: 'red',
+      headerTintColor: '#cbd5e1',
+      headerStyle: {
+        backgroundColor: '#0f172a'
+      }
     };
   };
   
