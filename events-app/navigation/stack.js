8c5c7b42e0f61e1229dee576017c79372ce7629f
rypptc <rypptc@gmail.com>
2024-08-06 00:30:59 -0400

    add navigation options, stack header is red

--- events-app/navigation/stack.js
+++ events-app/navigation/stack.js
@@ -1,14 +1,19 @@
+import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/home-screen';
 import EventDetailScreen from '../screens/event-detail-screen';
+import { navOptions } from './options';
 
 const Stack = createStackNavigator();
 
 export const HomeStack = () => {
+  const navigation = useNavigation();
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={() => navOptions(navigation)}
+    >
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Event" component={EventDetailScreen} />
     </Stack.Navigator>
   );
-}
+};
